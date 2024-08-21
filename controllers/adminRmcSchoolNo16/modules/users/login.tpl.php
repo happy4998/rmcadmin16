35738f9334d9c65ecbb0d8347d51d2cfbf359974
doshiyash001 <yash.doshi@stepinsolutions.in>
2024-08-21 11:20:40 +0530

    login

--- controllers/adminRmcSchoolNo16/modules/users/login.tpl.php
+++ controllers/adminRmcSchoolNo16/modules/users/login.tpl.php
@@ -3,13 +3,13 @@
 
 
 <head>
-    <title>Education Master Template</title>
+    <title>RMC School No 16</title>
     <!-- META TAGS -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Education master is one of the best educational html template, it's suitable for all education websites like university,college,school,online education,tution center,distance education,computer education">
-    <meta name="keyword" content="education html template, university template, college template, school template, online education template, tution center template">
+    <meta name="keyword" content="education html template, university template, college template, school template, online education template, tution center template"> -->
     <!-- FAV ICON(BROWSER TAB ICON) -->
     <link href="<?php echo getConfig('siteUrl').'/images/fav.ico'?>" rel="stylesheet" type="image/x-icon">
     <!-- GOOGLE FONT -->
@@ -27,37 +27,38 @@
 <body>
 
    <section>
-		<div class="ad-log-main">
-			<div class="ad-log-in">
-				<div class="ad-log-in-logo">
-					<a href="#"><img src="<?php echo getConfig('siteUrl').'/images/logo.png'?>" alt=""></a>
-				</div>
-				<div class="ad-log-in-con">
-			<div class="log-in-pop-right">
-                    <h4>Login</h4>
-                    <form class="s12" role="form" method="POST" action="<?php echo getConfig('siteUrl').'/users/login' ?>">
-                        <div>
-                            <div class="input-field s12">
-                                <input type="text" data-ng-model="name" class="validate" required="" name="username">
-                                <label class="">User name</label>
-                            </div>
+   <div class="ad-log-main">
+    <div class="ad-log-in">
+        <div class="ad-log-in-logo">
+            <img src="<?php echo getConfig('siteUrl').'/images/user/school-logo.jpeg'?>" alt="Logo" class="img-fluid" style="max-height: 90px;">
+        </div>
+        <div class="ad-log-in-con">
+            <div class="log-in-pop-right">
+                <h4>Login Here</h4>
+                <form class="s12" role="form" method="POST" action="<?php echo getConfig('siteUrl').'/users/login' ?>">
+                    <div>
+                        <div class="input-field s12" style="margin-bottom: 20px;">
+                            <input type="text" data-ng-model="name" class="validate" required="" name="username" style="height: 50px; font-size: 18px; width: 300px;">
+                            <label class="">User name</label>
                         </div>
-                        <div>
-                            <div class="input-field s12">
-                                <input type="password" class="validate" required="" name="password">
-                                <label>Password</label>
-                            </div>
+                    </div>
+                    <div>
+                        <div class="input-field s12" style="margin-bottom: 20px;">
+                            <input type="password" class="validate" required="" name="password" style="height: 50px; font-size: 18px; width: 300px;">
+                            <label>Password</label>
                         </div>
-                        <div>
-                            <div class="input-field s4">
-                                <!-- <i class="waves-effect waves-light log-in-btn waves-input-wrapper" style=""><input type="submit" value="Login" class="waves-button-input"></i> </div> -->
-                                <button type="submit" name="submit" id="loginBtn" class="btn btn-primary block full-width m-b"><?php echo __("Login")?></button>
+                    </div>
+                    <div>
+                        <div class="input-field s4">
+                            <button type="submit" name="submit" id="loginBtn" class="btn btn-primary block full-width m-b"><?php echo __("Login")?></button>
                         </div>
-                    </form>
-                </div>
-				</div>
-			</div>
-		</div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
    </section>
 
     <!--Import jQuery before materialize.js-->
